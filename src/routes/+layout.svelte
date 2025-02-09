a6db7156ed9e55472fc85f5b19fcc03b9892a3ef
carlos pacheco <aburender@gmail.com>
2025-02-09 13:51:26 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+
+	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+</script>
+
+<div>
+	<aside>
+		<nav>
+			<a href="/contador">Contador</a>
+		</nav>
+	</aside>
+	<main>
+		{@render children()}
+	</main>
+</div>
+
+<style>
+	:root {
+		font-family:
+			Roboto,
+			system-ui,
+			-apple-system,
+			BlinkMacSystemFont,
+			'Segoe UI',
+			Oxygen,
+			Ubuntu,
+			Cantarell,
+			'Open Sans',
+			'Helvetica Neue',
+			sans-serif;
+	}
+	div {
+		display: flex;
+	}
+	main {
+		display: grid;
+		place-content: center;
+	}
+	aside {
+		flex-basis: 14rem;
+		padding: var(--padding, 1rem);
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Bienvenidos a el proyecto de practica 7-GUIS</h1>
